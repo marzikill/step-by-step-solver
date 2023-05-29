9576f90e2f27aba57cc3ab15076bf1ea2688fe07
Florian Picard <florian.picard.fr@gmail.com>
2023-05-29 12:59:02 +0200

    problèmes minimum

---
+++ problèmes/minimum_iter.py
@@ -0,0 +1,53 @@
+import random
+from problem_solver import Problème, OutputException
+from data_structures import Entier, Liste
+from generate import alternate, mini_last, mini_first
+
+class TableauCaché(Liste):
+    def __init__(self, content, name = ""):
+        super().__init__(content, name)
+        self.indice_courant = 0
+
+    def incr(self):
+        """ TableauCaché -> None
+        Incrémente le curseur """
+        if self.indice_courant >= len(self.content) - 1:
+            raise OutputException("Le curseur est à la fin du tableau")
+        self.indice_courant += 1
+
+    def get(self):
+        return self.content[self.indice_courant]
+
+    def store(self):
+        """ TableauCaché -> Entier
+        Sauvegarde l'élément sous le curseur """
+        return Entier(self.get(), name = "mem")
+
+    def __repr__(self):
+        tab_str = [str(e).rjust(3) if i <= self.indice_courant
+                   else " X "
+                   for i, e in enumerate(self.content)]
+        curseur_str = ' '*(4*self.indice_courant + 2) + "^"
+        return f"\n{','.join(tab_str)}\n{curseur_str}"
+
+def gen(*args, **kwargs):
+    """ Fixe le type (Liste vers TableauCaché) et le nom renvoyés par la fonction alternate  """
+    gen_fun = alternate(mini_first, mini_last)
+    res = gen_fun(*args, **kwargs)
+    res = TableauCaché(res.content, name = 'tab')
+    return res
+
+def minimum(l):
+    """ TableauCaché -> Entier 
+    Étant donné un tableau tab non vide, renvoyer le plus petit élément de l """
+    # Les Listes héritent de ce qu'il faut :
+    m = min(l)
+    return Entier(m)
+
+                  
+if not __name__:
+    Problème(name = "Minimum (itératif)",
+             generating_fun = gen,
+             input_types = [],
+             problem_funs = [TableauCaché.incr, TableauCaché.store],
+             solution_fun = minimum)
---
+++ problèmes/minimum_rec.py
@@ -0,0 +1,20 @@
+import random
+from problem_solver import Problème
+from data_structures import Liste, Entier
+from generate import alternate, mini_last, mini_first
+
+@Problème.recursive(len)
+def minimum(l):
+    """ Liste -> Entier 
+    Étant donné une liste l non vide, renvoyer le plus petit élément de l """
+    # Les Listes héritent de ce qu'il faut :
+    m = min(l)
+    return m
+
+                  
+if not __name__:
+    Problème(name = "Minimum (recursif)",
+             generating_fun = alternate(mini_first, mini_last),
+             input_types = [],
+             problem_funs = [Liste.divise, Liste.ajoute],
+             solution_fun = minimum)
